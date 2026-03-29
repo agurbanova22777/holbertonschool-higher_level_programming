768fa5e013e01624298fd270b7b651375b56d5e2
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-03-29 22:42:52 +0400

    .

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module provides a function that adds two numbers as integers."""
+
+
+def add_integer(a, b=98):
+    """Return the sum of a and b after converting floats to integers."""
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
