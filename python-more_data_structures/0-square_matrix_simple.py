5f1f14015472f9dbf7feab48dcf05b57718e80f6
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-03-29 20:54:06 +0400

    .

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """Return a new matrix with each value squared (does not modify input)."""
     return [[value * value for value in row] for row in matrix]
