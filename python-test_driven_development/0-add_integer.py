e4aabe411be55c96e85b7dc7eaf29086bc98a704
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-03-29 23:31:56 +0400

    .

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-"""This module provides a function that adds two numbers as integers."""
+"""Module that defines add_integer."""
 
 
 def add_integer(a, b=98):
-    """Return the sum of a and b after converting floats to integers."""
+    """Return the addition of a and b.
+
+    Args:
+        a (int or float): First number.
+        b (int or float): Second number (default: 98).
+
+    Raises:
+        TypeError: If a is not an int or float.
+        TypeError: If b is not an int or float.
+
+    Returns:
+        int: The sum of a and b after casting floats to ints.
+    """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
