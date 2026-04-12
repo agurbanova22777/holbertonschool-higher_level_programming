46a694d44b04eae894a23aff24f4f4bc27d8d8b9
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-04-12 16:38:17 +0400

    .

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Function to return the list of available attributes and methods of an object."""
+"""Return the list of available attributes and methods of an object."""
 
 
 def lookup(obj):
-    """Return the list of available attributes and methods of obj."""
+    """Return a list of available attributes and methods of obj."""
     return dir(obj)
