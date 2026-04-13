792ce280a6439e162fbfa851fdc77329ac5a3663
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-04-13 12:07:18 +0400

    added a blank line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Read a text file and print to stdout"""
 
+
 def read_file(filename=""):
     """Reads a text file and print to stdout"""
     with open(filename, encoding="utf-8") as f:
