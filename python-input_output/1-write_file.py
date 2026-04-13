53910b673906fc0a8ec198f205ed3e089c5e6ac7
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-04-13 12:17:04 +0400

    fixed the doc and shebang

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
-#!usr/bin/python3
-"""Write a string to utf8 and return num of chars"""
+#!/usr/bin/python3
+"""This module Writes a string to utf8 and return num of chars"""
 
 
 def write_file(filename="", text=""):
+    """Write a string to utf8 and return num of chars"""
     with open(filename, 'w', encoding="utf8") as f:
         return f.write(text)
