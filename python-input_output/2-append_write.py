366b513208eaecc587fef10d74cb085284588f36
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-04-13 12:23:42 +0400

    fixed the name of prototype

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """This module appends a string to utf8 and returns num of chars"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Write a string to utf8 and return num of chars"""
     with open(filename, 'a', encoding="utf8") as f:
         return f.write(text)
