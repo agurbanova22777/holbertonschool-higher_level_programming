62c5db0056c8aaf7b057f51a20b26f56561f6548
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-04-19 13:43:58 +0400

    fixed d8 and 11 again pep8

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -12,12 +12,12 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dict representation of the Student (optionally filtered)."""
+        """Return a dict form of the Student (optionally filtered)."""
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replace attributes of the Student instance from a dictionary."""
+        """Replace Student attributes from a dictionary."""
         for key, value in json.items():
             setattr(self, key, value)
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Return a JSON-serializable dictionary description of an object."""
+"""Return a JSON-serializable dict description of an object."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return a dict description for JSON serialization."""
     return obj.__dict__
