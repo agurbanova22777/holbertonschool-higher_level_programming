a052250c2bcfb2759981ad9163fd67968684e544
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-02 17:52:32 +0400

    fixed syntax and indentation errors

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,22 +7,22 @@ import csv
 def fetch_and_print_posts():
     """Fetches all posts from JSONPlaceholder and prints their titles"""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f."Status Code: {response.status_code}")
-    if response.status_code = 200:
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
         posts = response.json()
-    for post in posts:
-        print(post.["title"])
+        for post in posts:
+            print(post["title"])
 
 def fetch_and_save_posts():
     """Fetches all posts from JSONPlaceholder and writes them into csv file"""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if response.status_code = 200:
+    if response.status_code == 200:
         posts = response.json()
         structured_posts = [
-            {"id": post.["id"], "title": post.["title"], "body": post.["body"]}
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
             for post in posts       
         ]
-    with open("posts.csv", mode="w", newline="", encoding="utf-8") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames = ["id", "title", "body"])
-        writer.writeheader()
-        writer.writerows(structured_posts)
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames = ["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(structured_posts)
