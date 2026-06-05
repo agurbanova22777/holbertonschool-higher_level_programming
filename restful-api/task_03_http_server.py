642297d0588a2ff1a2a56e951f98f617850ae6eb
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-05 17:31:01 +0400

    fixed the message for 404

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class SimpleServer(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 def run():
     """Start the server on port 8000"""
