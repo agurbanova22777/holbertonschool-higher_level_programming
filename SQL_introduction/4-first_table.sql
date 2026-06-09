a23e802607c11dce26be8d653dfac04096ffd010
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-06-09 16:52:03 +0400

    fixed exists to not exists

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- a script that creates a table called first_table in the current database in your MySQL server.
 -- write
-CREATE TABLE IF EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     Id INT,
     Name VARCHAR(256)
 );
