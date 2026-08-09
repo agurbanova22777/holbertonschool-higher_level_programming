03900dac0509d8dc89f2d31f83a307eec528c7ba
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-08-09 14:29:37 +0400

    .

---
+++ python-server_side_rendering/create_db.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Products (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            category TEXT NOT NULL,
+            price REAL NOT NULL
+        )
+    ''')
+    cursor.execute('''
+        INSERT INTO Products (id, name, category, price)
+        VALUES
+        (1, 'Laptop', 'Electronics', 799.99),
+        (2, 'Coffee Mug', 'Home Goods', 15.99)
+    ''')
+
+    cursor.execute("""
+        SELECT * FROM Products
+    """)
+
+    data = cursor.fetchall()
+    conn.commit()
+    conn.close()
+
+    return data
+
+def show():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    data = cursor.execute("""SELECT * FROM Products""")
+    data = data.fetchall()
+    return data
+
+if __name__ == '__main__':
+    create_database()
---
+++ python-server_side_rendering/parsers.py
@@ -0,0 +1,32 @@
+import json, csv
+import create_db
+
+def read_json_products():
+    with open("products.json", "r") as f:
+        return json.load(f)
+
+def read_csv_products():
+    products = []
+    with open("products.csv", newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            products.append({
+                "id": int(row["id"]),
+                "name": row["name"],
+                "category": row["category"],
+                "price": float(row["price"])
+            })
+    return products
+
+def read_database():
+    list_of_tables = ['id', 'name', 'category', 'price']
+    data = create_db.show()
+    products = []
+
+    for tup in data:
+        product_sample = {}
+        for index, i in enumerate(list_of_tables):
+            product_sample[i] = tup[index]
+        products.append(product_sample)
+
+    return products
